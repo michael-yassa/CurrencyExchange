70d495fb3623d33948b9b92ea6b41798349cf5c8
michael-yassa <egytec08@gmail.com>
2019-12-20 00:45:01 +0200

    arabic virsion

---
+++ app/src/main/java/com/MichaelProject/currencychange/Base/IconItems.java
@@ -0,0 +1,27 @@
+package com.MichaelProject.currencychange.Base;
+
+public class IconItems {
+
+    String iconName;
+    int imageUrl;
+
+    public IconItems(int imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getIconName() {
+        return iconName;
+    }
+
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
+
+    public int getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(int imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
